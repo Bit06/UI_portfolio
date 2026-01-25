5d032b9a28cec4c4a9ffb64a2dde7f9e471baa7f
David <daveair06@gmail.com>
2026-01-26 00:50:44 +0100

    perf: convert all images from PNG to JPG for 60% size reduction

--- components/Image/index.ts
+++ components/Image/index.ts
@@ -1,22 +1,22 @@
 // Portfolio Homepage Images
-const echoHomepage = new URL('./ECHO - Homepage.png', import.meta.url).href;
-const shopHebronHomepage = new URL('./SHOP HEBRON - Homepage.png', import.meta.url).href;
-const estateHomepage = new URL('./MY-ESTATE - Homepage.png', import.meta.url).href;
+const echoHomepage = new URL('./ECHO - Homepage.jpg', import.meta.url).href;
+const shopHebronHomepage = new URL('./SHOP HEBRON - Homepage.jpg', import.meta.url).href;
+const estateHomepage = new URL('./MY-ESTATE - Homepage.jpg', import.meta.url).href;
 
 // Profile Image
-const profileImage = new URL('./Profile image.png', import.meta.url).href;
+const profileImage = new URL('./Profile Image.jpg', import.meta.url).href;
 
 // Echo Case Study Images
-const echoFirstImage = new URL('./ECHO - Fist Image.png', import.meta.url).href;
-const echoSecondImage = new URL('./ECHO - Second Image.png', import.meta.url).href;
+const echoFirstImage = new URL('./ECHO - Fist Image.jpg', import.meta.url).href;
+const echoSecondImage = new URL('./ECHO - Second Image.jpg', import.meta.url).href;
 
 // Shop Hebron Case Study Images
-const shopHebronFirstImage = new URL('./SHOP HEBRON - First Image.png', import.meta.url).href;
-const shopHebronSecondImage = new URL('./SHOP HEBRON - Second Image.png', import.meta.url).href;
+const shopHebronFirstImage = new URL('./SHOP HEBRON - First Image.jpg', import.meta.url).href;
+const shopHebronSecondImage = new URL('./SHOP HEBRON - Second Image.jpg', import.meta.url).href;
 
 // Estate Case Study Images
-const estateFirstImage = new URL('./MY-ESTATE - First Image.png', import.meta.url).href;
-const estateSecondImage = new URL('./MY-ESTATE - Second Image.png', import.meta.url).href;
+const estateFirstImage = new URL('./MY-ESTATE - First Image.jpg', import.meta.url).href;
+const estateSecondImage = new URL('./MY-ESTATE - Second Image.jpg', import.meta.url).href;
 
 // Tagged image export for easy reference
 export const images = {
